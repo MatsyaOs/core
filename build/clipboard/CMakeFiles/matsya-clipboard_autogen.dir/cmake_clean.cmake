b858a46347e1e256b82a3aa225d9dd3f2157fcae
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-20 21:51:10 +0530

    ..

--- build/clipboard/CMakeFiles/matsya-clipboard_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/matsya-clipboard_autogen"
-  "matsya-clipboard_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/matsya-clipboard_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/xembed-sni-proxy/CMakeFiles/matsya-xembedsniproxy_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/matsya-xembedsniproxy_autogen"
-  "matsya-xembedsniproxy_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/matsya-xembedsniproxy_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
